4ea3edfca5cf352a401018bc1f256f4802328697
mayormcmatt <matthew.is.riding@gmail.com>
2014-06-20 21:13:10 -0700

    Again, no errors, but file not being saved

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,33 @@ class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  has_attached_file :image,
+    :path => ":rails_root/public/system/:class/:attachement/:id/:basename_:style.:extension",
+    :url => "/system/:class/:attachement/:id/:basename_:style.:extension",
+    :styles => {
+      :thumb    => ['100x100#',  :jpg, :quality => 70],
+      :preview  => ['480x480#',  :jpg, :quality => 70],
+      :large    => ['600>',      :jpg, :quality => 70],
+      :retina   => ['1200>',     :jpg, :quality => 30]
+    },
+    :convert_options => {
+      :thumb    => '-set colorspace sRGB -strip',
+      :preview  => '-set colorspace sRGB -strip',
+      :large    => '-set colorspace sRGB -strip',
+      :retina   => '-set colorspace sRGB -strip -sharpen 0x0.5'
+    }
+
+  validates_attachment_content_type :image,
+    :presence => true,
+    :size => { :in => 0..10.megabytes },
+    :content_type => { :content_type => /^image\/(jpeg|png|gif|tiff)$/ }
+
+  validates :image_file_name,
+    :presence => true,
+    :uniqueness => true
 
   def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect {|x| x.strip.downcase}.uniq
